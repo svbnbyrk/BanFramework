397b9536b0ca1450d5876399a8bf5943d132e61f
sevban.bayrak <sevban.bayrak@PC_SBAYRAK>
2019-01-08 09:56:04 +0300

    Dependecy Injection for Client Side Validiation

---
+++ BanFramwork.Mssql.Business/DependecyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,20 @@
+﻿using BanFramework.Mssql.Entities.Concrete;
+using BanFramwork.Mssql.Business.ValidationRules.FluentValidation;
+using FluentValidation;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanFramwork.Mssql.Business.DependecyResolvers.Ninject
+{
+    public class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Product>>().To<ProdactionValidatior>().InSingletonScope();
+        }
+    }
+}
